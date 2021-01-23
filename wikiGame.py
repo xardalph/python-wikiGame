a74667c80e011d37f7987403db223674de51d02d
evan <evan.adam@yahoo.fr>
2021-01-23 17:45:12 +0100

    utilisation du texte du lien plutôt que du href ajout d'un for pour trouver tous les liens présents dans la page

--- wikiGame.py
+++ wikiGame.py
@@ -17,8 +17,19 @@ def clear():
 
 
 def filter_url(url):
-    exclude_url = ("/wiki/Wikip", "/wiki/Fichier", "http", "/wiki/Portail", "/wiki/Spécial", "/wiki/Aide", "/wiki/501c", "#")
+    exclude_url = (
+    "/wiki/Wikip", "/wiki/Fichier", "http", "/wiki/Portail", "/wiki/Spécial", "/wiki/Aide", "/wiki/501c", "#")
     if url.startswith(exclude_url):
+        return False
+
+    # j'utilise des regex pour éviter les problèmes d'encodage des accent dans les noms des catégories (je ne sais pas ce que ca donnera dans d'autre langue par contre
+    if re.search('^/wiki/Cat.gorie:', url):
+        return False
+    if re.search('^/wiki/Discussion:', url):
+        return False
+    if re.search('^/wiki/Mod.le:', url):
+        return False
+    if re.search('^/wiki/Projet:', url):
         return False
 
     # certaine url ont deux // de suite, elle sont invalide, il faut les enlever
@@ -28,34 +39,34 @@ def filter_url(url):
         return False
 
     return True
+
+
 def print_all_link_store_hash(soup):
     array_link = []
-    for link in soup.find_all("a"):
-
-        if link.get('href') and filter_url(unquote(link.get('href'))):  # est-il valid ?
-            if link.get('href') not in array_link:  # est-il déjà présent dans le tableau
-                array_link.append(link.get('href'))
-                print("link   {}\t: {}".format(len(array_link) - 1, unquote(link.get('href'))))
+    for div in soup.find_all("main", {"id": "content"}):
+        for link in div.find_all("a"):
+            if link.get('href') and filter_url(unquote(link.get('href'))):  # est-il valid ?
+                if link.get('href') not in array_link:  # est-il déjà présent dans le tableau
+                    array_link.append(link.get('href'))
+                    print("link   {}\t: {}".format(len(array_link) - 1, unquote(link.get_text())))
 
     return array_link
+
 
 if __name__ == '__main__':
 
     random_url = "https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
     url = "https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
-    # url = urllib.request.urlopen(random_url)
+
     urlTarget = urllib.request.urlopen(random_url)
     nbCoup = 0
 
     while url != urlTarget.url:
-
         nbCoup += 1
 
         with urllib.request.urlopen(url) as response:
-
             soup = BeautifulSoup(response, 'html.parser')
             array_link = print_all_link_store_hash(soup)
-
 
         print("target is : ", unquote(urlTarget.url))
         print("Actual url is : ", unquote(response.url))
