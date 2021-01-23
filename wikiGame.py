65143a98d66dd138bf3183388022f67d1e6c6903
evan <evan.adam@yahoo.fr>
2021-01-23 16:09:26 +0100

    Revert wikiGame.py au commit 083b479d43e615286da30092fb16c4381f959a9a PyInquerer ne semble pas suffisament stable, plusieurs bug sont trop impactant pour pouvoir l'utiliser simplement et n'ont pas encore de patch

--- wikiGame.py
+++ wikiGame.py
@@ -1,10 +1,9 @@
-from __future__ import print_function, unicode_literals
 from bs4 import BeautifulSoup
 import urllib.request
-from urllib.parse import unquote, quote
+from urllib.parse import unquote
 from os import system, name
 import re
-from PyInquirer import prompt
+
 
 # define our clear function
 def clear():
@@ -18,60 +17,56 @@ def clear():
 
 
 def filter_url(url):
-    exclude_url = (
-    "/wiki/Wikip", "/wiki/Fichier", "http", "/wiki/Portail", "/wiki/Spécial", "/wiki/Aide", "/wiki/501c", "#")
+    exclude_url = ("/wiki/Wikip", "/wiki/Fichier", "http", "/wiki/Portail", "/wiki/Spécial", "/wiki/Aide", "/wiki/501c", "#")
     if url.startswith(exclude_url):
         return False
 
-
+    # certaine url ont deux // de suite, elle sont invalide, il faut les enlever
+    if re.search('//', url):
+        return False
     if "/w/index.php?" in url:
         return False
 
     return True
-
-
 def print_all_link_store_hash(soup):
-    local_array_link = set()
+    array_link = []
+    for link in soup.find_all("a"):
 
-    for div in soup.find_all("div", {"class": "mw-parser-output"}):
-        for link in div.find_all("a"):
-            if link.get('href') and filter_url(unquote(link.get('href'))):  # Le lien est-il valide ?
-                local_array_link.add(unquote(link.get('href')))
+        if link.get('href') and filter_url(unquote(link.get('href'))):  # est-il valid ?
+            if link.get('href') not in array_link:  # est-il déjà présent dans le tableau
+                array_link.append(link.get('href'))
+                print("link   {}\t: {}".format(len(array_link) - 1, unquote(link.get('href'))))
 
-    return local_array_link
-
-
-def get_question(array_link, url_target, actual_url):
-    message_to_send = 'which Link do you want to use ? target is : ' + url_target + ' actual url is : ' + actual_url
-    questions = [
-        {
-            'type': 'list',
-            'name': 'link_user',
-            'message': message_to_send,
-            'choices': array_link,
-        }
-    ]
-    return questions
-
+    return array_link
 
 if __name__ == '__main__':
 
     random_url = "https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
     url = "https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
+    # url = urllib.request.urlopen(random_url)
     urlTarget = urllib.request.urlopen(random_url)
     nbCoup = 0
-    clear()
 
     while url != urlTarget.url:
+
         nbCoup += 1
 
         with urllib.request.urlopen(url) as response:
+
             soup = BeautifulSoup(response, 'html.parser')
             array_link = print_all_link_store_hash(soup)
 
-        answers = prompt(get_question(array_link, unquote(urlTarget.url), unquote(response.url)))
 
-        url = "https://fr.wikipedia.org" + quote(answers.get('link_user'))
+        print("target is : ", unquote(urlTarget.url))
+        print("Actual url is : ", unquote(response.url))
+
+        while True:
+            userInput = int(input("please choose a link by it's number : "))
+            if len(array_link) > userInput:
+                break
+
+        url = "https://fr.wikipedia.org" + array_link[userInput]
+        print("url to go is {}".format(url))
         clear()
 
     print("YOU WIN!!")
